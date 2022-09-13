d0b094ebf974da83dbd11f548344944895d76a08
LAPTOP-RABTAUVD\apdlv <apdlvmf486@gmail.com>
2022-09-13 10:12:25 +0900

    단어 퍼즐 3단계
    랜덤 단어 선택 및 글자 순서를 랜덤하게 바꾸는 기능 추가

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-let word = document.getElementById('word').innerHTML;
+let word = document.getElementById('word');
 let wordBtn = document.getElementById('wordBtn');
 let checkWord = document.getElementById('check');
 let game = {};
-game.word = word.split('');
-game.btns = [];
+
 game.check = function(){
-    if (word === game.word.join('')){
+    if (word.innerHTML === game.word.join('')){
         checkWord.innerHTML = '일치합니다.';
     }else{
         checkWord.innerHTML = '일치하지 않습니다.';
     }
 }
 
-for (let i = 0; i < word.length; i++){
+game.sampleWords = ['apple', 'orange', 'banana', 'pineapple', 'kiwi', 'mango', 'pear', 'persimmon', 'blueberry'];
+game.choice = function(){
+    let idx = Math.floor(Math.random() *this.sampleWords.length);
+    return this.sampleWords[idx];
+}
+
+let answer = game.choice();
+word.innerHTML = answer;
+game.word = answer.split('');
+game.btns = [];
+
+for (let i = 0; i < word.innerHTML.length; i++){
     let btn = document.createElement('button');
-    btn.innerHTML = word[i];
+    btn.innerHTML = game.word[i];
     wordBtn.appendChild(btn);
     game.btns.push(btn);
 }
@@ -51,4 +61,13 @@ let push_right = function(event){
         game.btns[j].innerHTML = game.word[j];
     }
     game.check();
+}
+
+let isSuffle = Math.floor(Math.random() * 2) === 0;
+if (isSuffle){
+    swap();
+}
+
+for (let i = 0; i < Math.floor(Math.random() * game.btns.length); i++){
+    push_right();
 }
