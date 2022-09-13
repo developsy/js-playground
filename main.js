04829a0b00152322e35a4a0da9b62dba4ef39818
LAPTOP-RABTAUVD\apdlv <apdlvmf486@gmail.com>
2022-09-13 09:42:30 +0900

    단어 퍼즐 구현 2단계
    오른쪽 밀기/왼쪽 밀기/뒤집기 함수 구현 및 단어 일치 여부를 html 문서에 표시

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
 let word = document.getElementById('word').innerHTML;
 let wordBtn = document.getElementById('wordBtn');
+let checkWord = document.getElementById('check');
+let game = {};
+game.word = word.split('');
+game.btns = [];
+game.check = function(){
+    if (word === game.word.join('')){
+        checkWord.innerHTML = '일치합니다.';
+    }else{
+        checkWord.innerHTML = '일치하지 않습니다.';
+    }
+}
 
 for (let i = 0; i < word.length; i++){
     let btn = document.createElement('button');
     btn.innerHTML = word[i];
     wordBtn.appendChild(btn);
+    game.btns.push(btn);
 }
 
 let swap = function(event){
-    console.log('swap');
+    let reverse = [];
+
+    for (let k = 0; k < game.word.length; k++){
+        let letter = game.word.shift();
+        game.word.push(letter);
+        reverse.unshift(letter);
+    }
+
+    for (let j = 0; j < game.word.length; j++){
+        game.btns[j].innerHTML = reverse[j];
+    }
+    game.word = reverse;
+    game.check();
 }
 
-let push = function(event){
-    console.log('push');
+let push_left = function(event){
+    const letter = game.word.shift();
+    game.word.push(letter);
+    for (let j = 0; j < game.word.length; j++){
+        game.btns[j].innerHTML = game.word[j];
+    }
+    game.check();
+}
+
+let push_right = function(event){
+    const letter = game.word.pop();
+    game.word.unshift(letter);
+    for (let j = 0; j < game.word.length; j++){
+        game.btns[j].innerHTML = game.word[j];
+    }
+    game.check();
 }
